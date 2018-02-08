617c8cdfcf61628541952ef572d4da6a7f4923e4
bschneider29 <b.a.schneider229@gmail.com>
2018-02-08 11:43:22 -0500

    Add files via upload

---
+++ testtwitter.py
@@ -0,0 +1,52 @@
+from textblob import TextBlob #Must upload TextBlob first / pip install textblob
+import tweepy #Must upload tweepy first / pip install tweepy
+import matplotlib.pyplot as plt
+def tweet():
+    
+    consumer_key = 'DCy6o4Di0Ger2p6L22uG50abn' #API connection hash
+
+    consumer_secret= 'EaQFvNQsSrZ0OfDawlih4vPMoBAJLGir7LcHBrF0jcZNAufgD4' #Specifc hash to my (or a user) account
+
+    access_token='159693786-cpFUGrwuKBop7gzJjXqIVhsPi3Q14PzXx7ejfLTE' #Access hash
+
+    access_secret='S7DQtcvcyJ5HBixREglqCnltKFkRK3daWDjQgoNqrKOAz' #Connective hash
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret) #Grant authorization from my account to spyder 
+
+    auth.set_access_token(access_token, access_secret)
+
+    api = tweepy.API(auth) #Store authorization access code
+
+
+    
+
+    search_value = input("Enter Your Keyword or Twitter Handle: ") #Enter a keyword or a twitter handle
+    str(search_value) #Search input
+    tweets = api.search(search_value) #Search Twitter via API
+    for tweet in tweets:
+ 
+        print (tweet.text) #Print the tweets
+        analysis = TextBlob(tweet.text) #Provides sentiment analysis via TextBlob
+        y = analysis.subjectivity #Y value for graph
+        x = analysis.polarity #X value for graph
+        print(analysis.sentiment) #Print the sentiment under each tweet
+        if analysis.polarity > 0 and analysis.subjectivity >= 0: #My metric is 0>POSITIVE based on polarity 'emotion'
+            print("POSITIVE")
+        elif analysis.polarity == 0 and analysis.subjectivity >= 0: #My metric is 0==NEUTRAL 'emotion' based on polarity
+            print("NEUTRAL")     
+        else:
+            print("NEGATIVE") #My metric is 0<=NEGATIVE based on polarity 'emotion'
+        plt.figure(figsize=(6, 4)) #Graphs each senitmental analysis
+        plt.grid(True) # grid
+        plt.xlabel('x-axis')      #Label               
+        plt.ylabel('y-values')
+        plt.scatter(x,y) # Plotting 
+        plt.show 
+#Use Python Console for GUI graphs in exec file form
+def main():
+    tweet() 
+
+    
+if __name__ == "__main__": main() 
+
+#I did this project because my main interest is data science in social media 
